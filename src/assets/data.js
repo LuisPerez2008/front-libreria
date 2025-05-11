fec3c89131899cb8265832ed0ff727615672f34c
LuisPerez2008 <113653537+LuisPerez2008@users.noreply.github.com>
2025-05-11 00:09:51 -0500

    se arreglo data de rutas del carruser

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,12 +1,12 @@
 export const data = [
   {
-    id: 1, imgUrl: "src/assets/img/carrusel-1.jpg"
+    id: 1, imgUrl: "../src/assets/img/carrusel-1.jpg"
   },
   {
-    id: 2, imgUrl: "src/assets/img/carrusel-2.jpg"
+    id: 2, imgUrl: "../src/assets/img/carrusel-2.jpg"
   },
   {
-    id: 3, imgUrl: "src/assets/img/carrusel-3.jpg"
+    id: 3, imgUrl: "../src/assets/img/carrusel-3.jpg"
   },
   
 ];
